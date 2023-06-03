c2faece6fe4e657bff6dc7a2d2986318402af0ac
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2023-06-03 19:05:20 +0300

    Add files via upload

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,39 @@
 package com.example.filter;
 
-public class AuthFilter {
-    //write your code here!
+import com.example.servlet.LoginServlet;
+import com.example.servlet.LogoutServlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(servletNames = "LoginServlet")
+public class AuthFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        String path = req.getRequestURI();
+        if(path.contains("/user/")) {
+            Object user = req.getSession().getAttribute("user");
+        }
+        else {
+            resp.sendRedirect("login.jsp");
+        }
+        filterChain.doFilter(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
 }
