ab6dd8a07a1d65b06d6893d89d34b33024c00f4d
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2023-06-03 19:28:49 +0300

    Add files via upload

--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -13,11 +13,12 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/login.jsp").forward(req, resp);
         if(req.getSession().getAttribute("user") == null) {
             resp.sendRedirect("/login.jsp");
         }
         else {
-            resp.sendRedirect("user/hello.jsp");
+            resp.sendRedirect("/user/hello.jsp");
         }
     }
 
@@ -29,6 +30,7 @@ public class LoginServlet extends HttpServlet {
             resp.sendRedirect("/login.jsp");
         }
         else {
+            req.getSession().setAttribute("user", "user");
             resp.sendRedirect("user/hello.jsp");
         }
     }
